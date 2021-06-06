6279102e08e5bc0ab504acd9ed147636186f45b2
MMKaragoz <mmertkaragoz@gmail.com>
2021-06-06 16:26:25 +0300

    session

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,8 @@ exports.loginUser = async (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             // user session
-            res.status(200).send('You are logged in.');
+            req.session.userID = user._id;
+            res.status(200).redirect('/');
           }
         });
       }
