ac17222ea568a9c90e3930bc95e73055ff3fca27
MMKaragoz <mmertkaragoz@gmail.com>
2021-06-06 19:15:31 +0300

    dashboard

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ exports.loginUser = async (req, res) => {
           if (same) {
             // user session
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -42,5 +42,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
