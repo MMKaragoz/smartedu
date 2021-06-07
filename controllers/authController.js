fc7595eb1888907278978f3ab031eea8e33c47e6
MMKaragoz <mmertkaragoz@gmail.com>
2021-06-07 16:22:23 +0300

    roles are given and teacher can create courses

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,11 @@
 const User = require('../models/User');
+const Category = require('../models/Category');
 const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -47,8 +45,10 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID });
+  const categories = await Category.find();
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
+    categories,
   });
 };
