5f35957a46536f786778120786db36886ade00da
MMKaragoz <mmertkaragoz@gmail.com>
2021-06-08 11:21:27 +0300

    courses and students

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
@@ -21,11 +22,9 @@ exports.loginUser = async (req, res) => {
     await User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
-          if (same) {
-            // user session
-            req.session.userID = user._id;
-            res.status(200).redirect('/users/dashboard');
-          }
+          // user session
+          req.session.userID = user._id;
+          res.status(200).redirect('/users/dashboard');
         });
       }
     });
@@ -44,11 +43,16 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
   const categories = await Category.find();
+  const courses = await Course.find({ user: req.session.userID });
+
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
+    courses,
   });
 };
